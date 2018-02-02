0b6c96356e4046cbfb8aacb062090302f71cf593
paulwilljones <paul.jones17@homeoffice.gsi.gov.uk>
2018-02-02 16:15:46 +0000

    Added functionality to update status with remaining Pomodoro duration

--- slack-pom.py
+++ slack-pom.py
@@ -2,6 +2,7 @@
 import json
 import os
 import sys
+import time
 
 from slackclient import SlackClient
 
@@ -16,19 +17,19 @@ def set_snooze(minutes=25):
     return _process_response(api_call)
 
 
-def set_status(user, status='Pomming'):
-    profile = json.dumps({
-        "status_text": status,
-        "status_emoji": ":tomato:"
-    })
+def set_pomodoro_status():
+    timeLeft = 1500
+    while timeLeft > 0:
+        remaining = _get_remaining_pomo_duration()
+        status = "Currently in a Pomodoro. I'll be available in {} minutes" \
+            .format(remaining / 60)
+        _set_status(status, ":tomato:")
+        time.sleep(60)
+        timeLeft -= 60
 
-    api_call = sc.api_call(
-        'users.profile.set',
-        user=user,
-        profile=profile
-    )
 
-    return _process_response(api_call)
+def clear_status():
+    _set_status("", "")
 
 
 def _process_response(api_call):
@@ -38,9 +39,32 @@ def _process_response(api_call):
         raise Exception(api_call)
 
 
+def _get_remaining_pomo_duration():
+    api_call = sc.api_call('dnd.info')
+    return _process_response(api_call)
+
+
+def _set_status(status_text, status_emoji):
+    profile = json.dumps({
+        "status_text": status_text,
+        "status_emoji": status_emoji
+    })
+
+    api_call = sc.api_call(
+        'users.profile.set',
+        user=user,
+        profile=profile
+    )
+
+    response = _process_response(api_call)
+
+    return response['snooze_remaining']
+
+
 def main():
     set_snooze()
-    set_status(user)
+    set_pomodoro_status()
+    clear_status()
 
 
 if __name__ == "__main__":
